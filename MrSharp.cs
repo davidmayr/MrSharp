47e7eb1aae3fd271312ba736491d9bb565419822
David <davidliebtkekse@gmail.com>
2020-07-10 02:39:19 +0200

    Changed some logging stuff and began work on the command system

--- MrSharp.cs
+++ MrSharp.cs
@@ -32,21 +32,26 @@ namespace MrSharp
             {
                 TokenType = TokenType.Bot,
                 Token = this.ConfigManager.BaseConfig.DiscordConfig.Token,
-                AutoReconnect = this.ConfigManager.BaseConfig.DiscordConfig.AutoReconnect
+                AutoReconnect = this.ConfigManager.BaseConfig.DiscordConfig.AutoReconnect,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
             };
 
             DiscordClient = new DiscordClient(configuration);
             DiscordClient.Ready += HandleReady;
-
+            
+            
             CommandsNextConfiguration commandsConfiguration = new CommandsNextConfiguration
             {
-                
+                StringPrefix = this.ConfigManager.BaseConfig.SystemConfig.Prefix,
+                EnableDms = false,
+                EnableMentionPrefix = true
             };
 
             CommandsNextModule = DiscordClient.UseCommandsNext(commandsConfiguration);
             
             await DiscordClient.ConnectAsync();
-            
+
         }
 
         private Task HandleReady(ReadyEventArgs args)
